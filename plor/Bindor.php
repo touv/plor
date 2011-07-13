062158845913673cd81c455f25031a4db1563c02
Nicolas Thouvenin <nthouvenin@gmail.com>
2011-07-13 12:57:04 +0200

    oups

---
+++ plor/Bindor.php
@@ -0,0 +1,57 @@
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 fdm=marker :
+/**
+ * PLOR
+ *
+ * Copyright (c) 2010, Nicolas Thouvenin
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the author nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  PSO
+ * @package   PLOR
+ * @author    Nicolas Thouvenin <nthouvenin@gmail.com>
+ * @copyright 2010 Nicolas Thouvenin
+ * @license   http://opensource.org/licenses/bsd-license.php BSD Licence
+ */
+
+
+/**
+ * bindor Interface
+ *
+ * @category  Bindo
+ * @package   PLOR
+ * @author    Nicolas Thouvenin <nthouvenin@gmail.com>
+ * @copyright 2010 Nicolas Thouvenin
+ * @license   http://opensource.org/licenses/bsd-license.php BSD Licence
+ */
+interface Bindor
+{
+    public function bind($parameter, &$value, $data_type = PSO::PARAM_STR, $length = null);
+    public function bindValue($parameter, $value, $data_type = PSO::PARAM_STR, $length = null);
+    public function with($parameter, $data_type = PSO::PARAM_STR, $length = null);
+    public function set($parameter, $value);
+}
+
